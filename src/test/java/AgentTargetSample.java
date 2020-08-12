ca3fefcc83c65db3b5c7f5ec26b07a07d4a2e603
dkmos2016 <dkmos2016@126.com>
2020-08-12 17:50:34 +0800

    todo: fixup stub(idx 0/1)

--- src/test/java/AgentTargetSample.java
+++ src/test/java/AgentTargetSample.java
@@ -1,24 +1,56 @@
 import java.io.File;
 
 public class AgentTargetSample {
-    public static void output(String name) {
-        String msg = name;
-        System.out.println(String.format("%s say hello!", name));
+    private int id;
+    private String name;
+    public AgentTargetSample(int id, String name) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(id);
+        sb.append("|");
+        sb.append(name);
+        sb.toString();
+        this.id = id;
+        this.name = name;
     }
 
-    public static void output(String name, long code, File f) {
-        StringBuilder sb = new StringBuilder();
+    public String getName(String s) {
+        return s+name;
+    }
 
-        sb.append(name);
-        sb.append('|');
-        sb.append(code);
-        sb.append('|');
-        sb.append(f);
+    public static class test {
+        private int id;
+        private String desc;
+
+        public test(int id, String desc) {
+            this.id = id;
+            this.desc = desc;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d %s", this.id, this.desc);
+        }
+    }
+
+    public static void output(String[] strings, String name, test t, double i,int j) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(strings[0]);
+        sb.append("|");
+        sb.append(strings[1]);
+        sb.append("|");
+        sb.append(strings[2]);
+        sb.append("|");
+        sb.append(t.toString());
         System.out.println(sb.toString());
-        System.out.println(String.format("%s say hello!", name));
+
+        System.out.println(String.format("%s say hello!",name));
     }
 
     public static void main(String[] args) {
-        output("len");
+        String[] strings = new String[] {"hello", "world", "!"};
+        test t = new test(1, "HHHHHH");
+        output(strings, "hhhh",  t, 1, 1);
+        System.out.println(args.length);
     }
 }
