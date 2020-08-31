a8479353c8fb365e7590232c3b547e71229bd6ad
dkmos2016 <dkmos2016@126.com>
2020-08-31 13:52:15 +0800

    暂时无方案修复支持ReqInfo中加载HttpServletRequest，总会导致各ClassLoader加载冲突的问题，留待以后解决

--- src/main/java/SecAgent/utils/AgentClassLoader.java
+++ src/main/java/SecAgent/utils/AgentClassLoader.java
@@ -13,24 +13,14 @@ public class AgentClassLoader extends ClassLoader {
   protected ClassLoader parent;
 
   protected AgentClassLoader(ClassLoader parent) {
-
-    System.out.println("use AgentClassLoader.....");
-    System.out.println(this.getClass().getClassLoader());
-    if (parent != null) {
-      this.parent = parent;
-      return;
-    }
-
-    ClassLoader p = getParent();
-    if (p == null) {
-      p = getSystemClassLoader();
-    }
-    this.parent = p;
+    super(parent);
+//    this.parent = parent;
   }
 
   @Override
   protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
     System.out.println("AgentClassLoader loading class ");
+    System.out.println(this.getParent());
     return super.loadClass(name, resolve);
   }
 }
--- src/main/java/SecAgent/utils/ReqInfo.java
+++ src/main/java/SecAgent/utils/ReqInfo.java
@@ -1,19 +1,11 @@
 package SecAgent.utils;
 
-import javax.servlet.http.HttpServletRequest;
+
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.URLClassLoader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReqInfo {
-  private AgentClassLoader agentClassLoader;
-
-  // store httpservletrequest
-  private HttpServletRequest httpServletRequest;
-
   // store throwable & parameter
   private final Map<String, StubData> StubDatas = new HashMap<>();
 
@@ -41,89 +33,13 @@ public class ReqInfo {
     System.out.println(this.getClass().getClassLoader());
   }
 
-  public void setHttpServletRequest(HttpServletRequest httpServletRequest) {
-
-    System.out.println("setHttpServletRequest: ");
-    agentClassLoader = new AgentClassLoader(Thread.currentThread().getContextClassLoader());
-
-    System.out.println(this.agentClassLoader.getParent());
-    this.httpServletRequest = httpServletRequest;
-    for (int i=0;i < 3; i++) {
-      if (i == 0) {
-        agentClassLoader = new AgentClassLoader(Thread.currentThread().getContextClassLoader());
-      } else if (i == 1) {
-        agentClassLoader = new AgentClassLoader(this.getClass().getClassLoader());
-      } else {
-        agentClassLoader = new AgentClassLoader(httpServletRequest.getClass().getClassLoader());
-      }
-      try {
-        initExtra();
-        break;
-      } catch (Exception e) {
-        e.printStackTrace();
-        continue;
-      }
-    }
-
-  }
-
-  /**
-   * generate other fields from httpServletRequest
-   */
-  public void initExtra() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    Class cls = Class.forName("javax.servlet.http.HttpServletRequest", true, agentClassLoader);
-
-    Class [] paraTypes = new Class[]{};
-    Object [] paras = new Object[]{};
-
-    String scheme;
-    String host;
-    int port;
-    String uri;
-
-    // setUrl
-    Method method = cls.getMethod("getScheme", new Class[]{});
-
-//      initExtra();
-
-    method = cls.getMethod("getScheme", paraTypes);
-    scheme = (String)method.invoke(httpServletRequest, paras);
-
-    method = cls.getMethod("getServerName", paraTypes);
-    host = (String)method.invoke(httpServletRequest, paras);
-
-    method = cls.getMethod("getServerPort", paraTypes);
-    port = (int)method.invoke(httpServletRequest, paras);
-
-    method = cls.getMethod("getRequestURI", paraTypes);
-    uri = (String) method.invoke(httpServletRequest, paras);
-
-    method = cls.getMethod("getMethod", paraTypes);
-    this.method = (String) method.invoke(httpServletRequest, paras);
-
-
-    this.url = String.format("%s://%s:%d%s", scheme,
-      host, port, uri);
-
-    method = cls.getMethod("getParameterMap", paraTypes);
-    this.queries = (Map)method.invoke(httpServletRequest, paras);
-
-    System.out.println(this.method);
-    System.out.println(this.url);
-    System.out.println(this.queries);
-
-    // todo
-    // this.headers
+  public void setUrl(String url) {
+    this.url = url;
   }
 
   public void putStubData(String type, Throwable throwable, Object obj) {
-    try {
-      Class cls = Class.forName("javax.servlet.http.HttpServletRequest", true, agentClassLoader);
-      System.out.println("putstubdata: " + type);
+      System.out.println("putstubdata: " + type + ", throwable: " + throwable + ", obj");
       StubDatas.put(type, new StubData(throwable, obj));
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    }
   }
 
   public String processStubData() {
