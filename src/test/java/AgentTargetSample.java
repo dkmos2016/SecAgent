db74a6883061d0f2541edc84e15e537c2faff186
dkmos2016 <dkmos2016@126.com>
2020-09-14 14:15:05 +0800

    基本使用原生httpurlconnection实现http请求，添加base64算法源码

--- src/test/java/AgentTargetSample.java
+++ src/test/java/AgentTargetSample.java
@@ -1,11 +1,11 @@
-import SecAgent.Logger.ExceptionLogger;
-import SecAgent.utils.AgentClassLoader;
+import SecAgent.utils.Common;
 import SecAgent.utils.ReqInfo;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 public class AgentTargetSample<E> {
@@ -43,15 +43,41 @@ public class AgentTargetSample<E> {
   }
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    System.out.println("main start");
-    ExceptionLogger.doTestAsync(new Exception("select id, value from test where id = 1"));
-    ExceptionLogger.doTestAsync(new Exception("select id, value from test where id = 2"));
-    ExceptionLogger.doTestAsync(new Exception("select id, value from test where id = 3"));
-    //    ExceptionLogger.doTestAsync(new Exception("error test4"));
-    //    ExceptionLogger.doTestAsync(new Exception("error test5"));
-    ExceptionLogger.Shutdown();
-    System.out.println("main done");
-    new AgentTargetSample().test1(true, new short[]{1,2}, (byte) 1, 1, 1, 1, 1.0, null);
+//    System.out.println("main start");
+//    ExceptionLogger.doTestAsync(new Exception("select id, value from test where id = 1"));
+//    ExceptionLogger.doTestAsync(new Exception("select id, value from test where id = 2"));
+//    ExceptionLogger.doTestAsync(new Exception("select id, value from test where id = 3"));
+//    //    ExceptionLogger.doTestAsync(new Exception("error test4"));
+//    //    ExceptionLogger.doTestAsync(new Exception("error test5"));
+//    ExceptionLogger.Shutdown();
+//    System.out.println("main done");
+//    new AgentTargetSample().test1(true, new short[]{1,2}, (byte) 1, 1, 1, 1, 1.0, null);
+
+//    HttpClientHelper.doGet("https://www.baidu.com");
+
+    HashMap headers = new HashMap();
+    headers.put("Content-Type", "application/json; charset=utf-8");
+
+    HashMap body = new HashMap();
+    body.put("name", "test");
+    body.put("password", "123456");
+
+    ArrayList arrayList = new ArrayList();
+    arrayList.add(1);
+    arrayList.add(2);
+    arrayList.add(3);
+
+    body.put("tetst", arrayList);
+
+    body.put("tttt", new Object[] {"4", "5", 6});
+
+    body.put("tttt1", true);
+
+
+    System.out.println(Common.MapToJsonStr(body));
+
+
+//    HttpClientHelper.doPost("http://127.0.0.1:8888", headers, body);
   }
 
   public  void test1(boolean g, short a[], byte c, int i, long l, float f, double d, String []aa) {
