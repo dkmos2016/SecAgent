5f9e3200851b1767f9453472947e218ce435ad2b
len <dkmos2016@126.com>
2020-10-03 01:01:48 +0800

    漏提交Config，现补充

--- src/main/java/SecAgent/Conf/Config.java
+++ src/main/java/SecAgent/Conf/Config.java
@@ -10,14 +10,14 @@ public class Config {
   public static final String INFORMATION_PATH = Resources.getProperty("EXCEPTION_LOG_PATH");
 
   public static final String SQL_STUB =
-      "com.mysql.cj.jdbc.EscapeProcessor.escapeSQL(Ljava.lang.String;Ljava.util.TimeZone;ZZLcom.mysql.cj.exceptions.ExceptionInterceptor;)Ljava.lang.Object;";
+    "com.mysql.cj.jdbc.EscapeProcessor.escapeSQL(Ljava.lang.String;Ljava.util.TimeZone;ZZLcom.mysql.cj.exceptions.ExceptionInterceptor;)Ljava.lang.Object;";
 
   // bug
   //  public static final String MYSQL_STUB =
   //      "com.mysql.cj.jdbc.StatementImpl.executeInternal(Ljava.lang.String;Z)Z";
 
   public static final String MYSQL_STUB =
-      "com.mysql.cj.jdbc.StatementImpl.checkNullOrEmptyQuery(Ljava.lang.String;)V";
+    "com.mysql.cj.jdbc.StatementImpl.checkNullOrEmptyQuery(Ljava.lang.String;)V";
 
   //  public static final String MYSQL_STUB =
   //
@@ -27,25 +27,32 @@ public class Config {
   //      "oracle.jdbc.driver.OracleStatement.executeInternal(Ljava.lang.String;)Z";
 
   public static final String ORACLE_STUB =
-      "oracle.jdbc.driver.OracleSql.initialize(Ljava.lang.String;)V";
+    "oracle.jdbc.driver.OracleSql.initialize(Ljava.lang.String;)V";
 
-  public static final String MYBATIS_STUB =
-      "org.apache.ibatis.mapping.BoundSql.<init>(Lorg.apache.ibatis.session.Configuration;Ljava.lang.String;Ljava.util.List;Ljava.lang.Object;)V";
+  public static final String MYBATIS_SQLA_STUB =
+    "org.apache.ibatis.scripting.xmltags.TextSqlNode.apply(Lorg.apache.ibatis.scripting.xmltags.DynamicContext;)Z";
+
+  public static final String MYBATIS_SQLB_STUB =
+    "org.apache.ibatis.scripting.xmltags.DynamicContext.appendSql(Ljava.lang.String;)V";
+
+  public static final String MYBATIS_VALUE_STUB =
+    "org.apache.ibatis.scripting.xmltags.TextSqlNode$BindingTokenParser.handleToken(Ljava.lang.String;)Ljava.lang.String;";
+
 
   //    public final static String SQL_STUB = "java.sql.Statement.execute(Ljava.lang.String;)Z";
   public static final String EXEC_STUB =
-      "java.lang.ProcessImpl.start([Ljava.lang.String;Ljava.util.Map;Ljava.lang.String;[Ljava.lang.ProcessBuilder$Redirect;Z)Ljava.lang.Process;";
+    "java.lang.ProcessImpl.start([Ljava.lang.String;Ljava.util.Map;Ljava.lang.String;[Ljava.lang.ProcessBuilder$Redirect;Z)Ljava.lang.Process;";
   public static final String DOWN_STUB = "java.io.FileInputStream.<init>(Ljava.io.File;)V";
   public static final String UPLOAD_STUB = "java.io.FileOutputStream.<init>(Ljava.io.File;Z)V";
   //    public static final String SPRING_URL_STUB =
   // "org.springframework.web.servlet.DispatcherServlet.doService(Ljavax.servlet.http.HttpServletRequest;Ljavax.servlet.http.HttpServletResponse;)V";\
   public static final String SPRING_URL_STUB =
-      "javax.servlet.http.HttpServlet.service(Ljavax.servlet.http.HttpServletRequest;Ljavax.servlet.http.HttpServletResponse;)V";
+    "javax.servlet.http.HttpServlet.service(Ljavax.servlet.http.HttpServletRequest;Ljavax.servlet.http.HttpServletResponse;)V";
 
   // doing
 
   public static final String XXE_STUB =
-      "com.sun.org.apache.xerces.internal.impl.XMLEntityManager$RewindableInputStream.<init>(Lcom.sun.org.apache.xerces.internal.impl.XMLEntityManager;Ljava.io.InputStream;)V";
+    "com.sun.org.apache.xerces.internal.impl.XMLEntityManager$RewindableInputStream.<init>(Lcom.sun.org.apache.xerces.internal.impl.XMLEntityManager;Ljava.io.InputStream;)V";
   //  public static final String
   // XXE_STUB="com.sun.org.apache.xerces.internal.impl.XMLVersionDetector.determineDocVersion(Lcom.sun.org.apache.xerces.internal.xni.parser.XMLInputSource;)S";
 
@@ -53,7 +60,7 @@ public class Config {
   public static final String SSRF_STUB = "java.io.FileOutputStream.<init>(Ljava.io.File;Z)V";
 
   public static final String TOMCAT_STUB =
-      "org.apache.tomcat.util.http.fileupload.FileUploadBase.parseRequest(Lorg.apache.tomcat.util.http.fileupload.RequestContext;)Ljava.util.List;";
+    "org.apache.tomcat.util.http.fileupload.FileUploadBase.parseRequest(Lorg.apache.tomcat.util.http.fileupload.RequestContext;)Ljava.util.List;";
 
   public static String[] exclude_classes;
   public static String[] exclude_methods;
@@ -62,78 +69,90 @@ public class Config {
 
   static {
     exclude_classes =
-        new String[] {
-          "MethodHandleImpl", "ClassValue", "ExceptionInInitializerError",
-        };
+      new String[]{
+        "MethodHandleImpl", "ClassValue", "ExceptionInInitializerError",
+      };
 
-    exclude_methods = new String[] {"main", "<clinit>"};
+    exclude_methods = new String[]{"main", "<clinit>"};
     include_classes =
-        new String[] {
-          "java.io",
-          // xxe
-          "com.sun.org.apache.xerces.internal.impl.XMLEntityManager",
-          "com.sun.org.apache.xerces.internal.impl.XMLEntityManager$RewindableInputStream",
-          // url
-          "javax.servlet.http.HttpServlet",
-          // down
-          //          "java.io.FileInputStream",
-          // upload
-          //          "java.io.FileOutputStream",
-          // exec
-          "java.lang.ProcessImpl",
+      new String[]{
+        "java.io",
+        // xxe
+        "com.sun.org.apache.xerces.internal.impl.XMLEntityManager",
+        "com.sun.org.apache.xerces.internal.impl.XMLEntityManager$RewindableInputStream",
+        // url
+        "javax.servlet.http.HttpServlet",
+        // down
+        //          "java.io.FileInputStream",
+        // upload
+        //          "java.io.FileOutputStream",
+        // exec
+        "java.lang.ProcessImpl",
 
-          // mysql
-          "com.mysql.cj.jdbc.StatementImpl",
+        // mysql
+        "com.mysql.cj.jdbc.StatementImpl",
 
-          /* ignore oracle because of ojdbc6 exception */
-          "oracle.jdbc.driver.OracleStatement",
-          //            "oracle.jdbc.driver.OracleResultSetImpl",
+        /* ignore oracle because of ojdbc6 exception */
+        "oracle.jdbc.driver.OracleStatement",
+        //            "oracle.jdbc.driver.OracleResultSetImpl",
 
-          /* mybatis */
-          "org.apache.ibatis.mapping.BoundSql",
-          "org.apache.ibatis.scripting.xmltags.OgnlCache",
-          "org.apache.ibatis.ognl.Ognl",
-          "org.apache.ibatis.parsing.GenericTokenParser",
-          //            "org.apache.ibatis.mapping.SqlSource",
+        /* mybatis */
+        "org.apache.ibatis.mapping.BoundSql",
+        "org.apache.ibatis.scripting.xmltags.OgnlCache",
+        "org.apache.ibatis.ognl.Ognl",
+        "org.apache.ibatis.parsing.GenericTokenParser",
+        //            "org.apache.ibatis.mapping.SqlSource",
 
-          // tomcat
-          "org.apache.tomcat.util.http.fileupload.FileUploadBase",
-          "oracle.jdbc.driver.OracleSql",
-        };
+        // tomcat
+        "org.apache.tomcat.util.http.fileupload.FileUploadBase",
+        "oracle.jdbc.driver.OracleSql",
+
+//          "org.apache.ibatis.mapping.MappedStatement",
+//          "org.apache.ibatis.scripting.xmltags.MixedSqlNode",
+        "org.apache.ibatis.scripting.xmltags.TextSqlNode",
+        "org.apache.ibatis.scripting.xmltags.TextSqlNode$BindingTokenParser",
+        "org.apache.ibatis.scripting.xmltags.DynamicContext",
+      };
 
     include_methods =
-        new String[] {
-          //          "len.test.show2",
-          //          "java.lang.SecurityManager.checkWrite(Ljava.lang.String;)V",
-          //          "java.lang.Runtime.exec",
-          //          "java.sql.Statement",
-          //          "java.lang.UNIXProcess",
-          //          "com.sun.org.apache.xerces.internal.impl.XMLVersionDetector",
-          //
-          // "com.sun.org.apache.xerces.internal.impl.XMLEntityManager$RewindableInputStream.<init>",
-          //                          "java.sql.Statement.execute",
-          //                    "com.mysql.cj.jdbc.StatementImpl",
-          //
-          // "com.mysql.cj.jdbc.EscapeProcessor.escapeSQL(Ljava.lang.String;Ljava.util.TimeZone;ZZLcom.mysql.cj.exceptions.ExceptionInterceptor;)Ljava.lang.Object;",
-          //                          "oracle.jdbc.driver.OracleStatement.executeInternal",
-          //                "oracle.jdbc.driver.OracleStatement.execute",
+      new String[]{
+        //          "len.test.show2",
+        //          "java.lang.SecurityManager.checkWrite(Ljava.lang.String;)V",
+        //          "java.lang.Runtime.exec",
+        //          "java.sql.Statement",
+        //          "java.lang.UNIXProcess",
+        //          "com.sun.org.apache.xerces.internal.impl.XMLVersionDetector",
+        //
+        // "com.sun.org.apache.xerces.internal.impl.XMLEntityManager$RewindableInputStream.<init>",
+        //                          "java.sql.Statement.execute",
+        //                    "com.mysql.cj.jdbc.StatementImpl",
+        //
+        // "com.mysql.cj.jdbc.EscapeProcessor.escapeSQL(Ljava.lang.String;Ljava.util.TimeZone;ZZLcom.mysql.cj.exceptions.ExceptionInterceptor;)Ljava.lang.Object;",
+        //                          "oracle.jdbc.driver.OracleStatement.executeInternal",
+        //                "oracle.jdbc.driver.OracleStatement.execute",
 
-          // mybatis
-          //                "org.apache.ibatis.mapping.BoundSql",
-          //                "org.apache.ibatis.mapping.SqlSource",
+        // mybatis
+        //                "org.apache.ibatis.mapping.BoundSql",
+        //                "org.apache.ibatis.mapping.SqlSource",
 
-          //
-          // "org.apache.tomcat.util.http.fileupload.FileUploadBase.parseRequest(Lorg.apache.tomcat.util.http.fileupload.RequestContext;)Ljava.util.List;",
-          //          "org.apache.ibatis.scripting.xmltags.OgnlCache.parseExpression",
+        //
+        // "org.apache.tomcat.util.http.fileupload.FileUploadBase.parseRequest(Lorg.apache.tomcat.util.http.fileupload.RequestContext;)Ljava.util.List;",
+        //          "org.apache.ibatis.scripting.xmltags.OgnlCache.parseExpression",
 
-          // sql in xxMapper.xml value(${id}...)
-          //          "org.apache.ibatis.ognl.Ognl.getValue",
+        // sql in xxMapper.xml value(${id}...)
+//                    "org.apache.ibatis.ognl.Ognl.getValue",
 
-          // sql in xxMapper.xml (process twice, #/$)
-          //          "org.apache.ibatis.parsing.GenericTokenParser.parse",
+        // sql in xxMapper.xml (process twice, #/$)
+//                    "org.apache.ibatis.parsing.GenericTokenParser.parse",
 
-          //                "oracle.jdbc.driver.OracleSql.initialize",
-        };
+        //                "oracle.jdbc.driver.OracleSql.initialize",
+
+//          "org.apache.ibatis.mapping.MappedStatement.getBoundSql",
+//          "org.apache.ibatis.scripting.xmltags.MixedSqlNode.apply",
+//          "org.apache.ibatis.scripting.xmltags.TextSqlNode.apply"
+        "org.apache.ibatis.scripting.xmltags.DynamicContext.appendSql",
+//          "org.apache.ibatis.scripting.xmltags.TextSqlNode$BindingTokenParser.handleToken",
+      };
   }
 
   public static boolean isExcludedClass(String src) {
