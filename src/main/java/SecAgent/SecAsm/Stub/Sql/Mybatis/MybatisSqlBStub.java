b37fce7d0817dee4729a0f51141cec648fc62fff
len <dkmos2016@126.com>
2020-10-04 23:31:27 +0800

    优化Mybatis记录方式，方便日志记录整理

--- src/main/java/SecAgent/SecAsm/Stub/Sql/Mybatis/MybatisSqlBStub.java
+++ src/main/java/SecAgent/SecAsm/Stub/Sql/Mybatis/MybatisSqlBStub.java
@@ -20,24 +20,20 @@ public class MybatisSqlBStub extends CommonStub {
   }
 
   private void process() {
-    debug_print_offline(String.format("[DEBUG] [MybatisSqlBStub]: %s", this.paramsInfo.toString()));
+    newArrayList(params2_idx);
+    addListElement(params2_idx, T_OBJECT, 0);
+
+    mv.visitLdcInsn("AFTER");
+    mv.visitVarInsn(ASTORE, tmp_obj);
+    addListElement(params2_idx, T_OBJECT, tmp_obj);
 
     mv.visitVarInsn(ALOAD, 1);
     mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "trim", "()Ljava/lang/String;", false);
     mv.visitVarInsn(ASTORE, tmp_obj);
 
-    debug_print_online(T_OBJECT, 0);
-    debug_print_online(T_OBJECT, tmp_obj);
-
-    newArrayList(params2_idx);
-    addListElement(params2_idx, T_OBJECT, 0);
     addListElement(params2_idx, T_OBJECT, tmp_obj);
 
-    addListElement(params2_idx, T_OBJECT, params2_idx);
     putStubData("MyBatis", T_OBJECT, params2_idx);
-
-
-//    putStubData("SQL", T_OBJECT, 2);
   }
 
   @Override
