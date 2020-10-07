6a059bdf2dd5084122bc270039766fd4a7551e92
len <dkmos2016@126.com>
2020-10-07 23:05:09 +0800

    存放埋点相关信息

--- src/main/java/SecAgent/Conf/StubConfig.java
+++ src/main/java/SecAgent/Conf/StubConfig.java
@@ -2,9 +2,7 @@ package SecAgent.Conf;
 
 import SecAgent.utils.Resources;
 
-public class Config {
-  public static final boolean DEBUG = true;
-
+public class StubConfig {
   public static final String DEBUG_PATH = Resources.getProperty("DEBUG_LOG_PATH");
   public static final String EXCEPTION_PATH = Resources.getProperty("INFORMATION_LOG_PATH");
   public static final String INFORMATION_PATH = Resources.getProperty("EXCEPTION_LOG_PATH");
