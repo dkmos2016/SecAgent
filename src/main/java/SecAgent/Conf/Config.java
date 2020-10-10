e13d522cefef41ed6ca26d4e27b671bd83864b67
dkmos2016 <dkmos2016@126.com>
2020-10-10 17:48:26 +0800

    优化测试代码

--- src/main/java/SecAgent/Conf/Config.java
+++ src/main/java/SecAgent/Conf/Config.java
@@ -4,7 +4,8 @@ import SecAgent.utils.DefaultLoggerHelper.DefaultLogger;
 import SecAgent.utils.Resources;
 
 public class Config {
-  public final static boolean ALLOWED_DIY_DEBUG;
+  public final static boolean DEBUG;
+  public final static boolean ALLOWED_DIY_LEVEL;
   public static final String DEFAULT_LOGGER_NAME;
   public final static DefaultLogger.MyLevel DEFAULT_LOGGER_LEVEL;
 
@@ -14,12 +15,14 @@ public class Config {
 
 
   static {
-    ALLOWED_DIY_DEBUG = false;
+    DEBUG = false;
+    ALLOWED_DIY_LEVEL = false;
+
     DEFAULT_LOGGER_NAME = "SecAgent";
     DEFAULT_LOGGER_LEVEL = DefaultLogger.MyLevel.INFO;
 
     DEBUG_PATH = Resources.getProperty("DEBUG_LOG_PATH");
-    EXCEPTION_PATH = Resources.getProperty("INFORMATION_LOG_PATH");
-    INFORMATION_PATH = Resources.getProperty("EXCEPTION_LOG_PATH");
+    EXCEPTION_PATH = DEBUG? Resources.getProperty("INFORMATION_LOG_PATH"): (Resources.getProperty("LOG_DIR") + Resources.getProperty("INFORMATION_LOG_PATH"));
+    INFORMATION_PATH = DEBUG? Resources.getProperty("INFORMATION_LOG_PATH"): Resources.getProperty("LOG_DIR") + Resources.getProperty("EXCEPTION_LOG_PATH");
   }
 }
